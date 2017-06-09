81d23508792669cb2ce1188ecc4da3f3fd38df8e
Harco Kuppens <h.kuppens@cs.ru.nl>
2017-06-09 17:05:23 +0200

    improved kernel interrupt handler with counter

--- examples/xenomai3/kernel_modules/test-irq-in-kernel-mode-using-gpiolib/irq-gpio-rtdm.c
+++ examples/xenomai3/kernel_modules/test-irq-in-kernel-mode-using-gpiolib/irq-gpio-rtdm.c
@@ -4,25 +4,28 @@
 #include <linux/module.h>
 
 //#include <linux/list.h>
-#include <rtdm/driver.h>
-#include <rtdm/uapi/gpio.h>
-//#include <rtdm/rtdm_driver.h>
+#include <rtdm/driver.h> // replaces #include <rtdm/rtdm_driver.h> in xenomai 2
+//#include "rtdk.h" // obsolete!!
 
-// GPIO IN 23 -> Broche 16
-#define GPIO_IN  17
-// GPIO OUT 22 -> Broche 15
+#define GPIO_IN  23
 #define GPIO_OUT 22
 
 static rtdm_irq_t irq_rtdm;
 
+// globalCounter:
+//   Global variable to count interrupts
+//   Should be declared volatile to make sure the compiler doesn't cache it.
+//
+static volatile int globalCounter = 0 ;
 
 static int handler_interruption(rtdm_irq_t * irq)
 {
-	static int value = 0;
-    printk("interrupt\n");
-	gpio_set_value(GPIO_OUT, value);
+    static int value = 0;
+	gpio_set_value(GPIO_OUT, value);  // realtime ???
 	value = 1 - value;
-	return RTDM_IRQ_HANDLED;
+    //  printk("interrupt\n"); -> not allowed in irq handler in linux, and certrainly not  realtime
+	 ++globalCounter ;
+    return RTDM_IRQ_HANDLED;
 }
 
 
@@ -31,7 +34,8 @@ static int __init exemple_init (void)
 	int err;
 
 	int numero_interruption = gpio_to_irq(GPIO_IN);
-	
+	printk("numero_interruption %d\n",numero_interruption);  
+
     printk("gpio request in\n");
 	if ((err = gpio_request(GPIO_IN, THIS_MODULE->name)) != 0) {
 		printk("error %d\n",err);
@@ -56,14 +60,17 @@ static int __init exemple_init (void)
 		gpio_free(GPIO_IN);
 		return err;
 	}
+       
+    // initially turn output pin ON -> see module loaded!!
+    gpio_set_value(GPIO_OUT, 1);
 
     printk("set irq trigger: rising\n");
-	irq_set_irq_type(numero_interruption,  IRQF_TRIGGER_RISING);
+        irq_set_irq_type(numero_interruption,  IRQ_TYPE_EDGE_FALLING);
 
-    printk("irq request , IRQTYPE_EDGE\n");
+    printk("irq request , IRQ_TYPE_EDGE_FALLING\n");
 	if ((err = rtdm_irq_request(& irq_rtdm, 
 	                 numero_interruption, handler_interruption, 
-	                 RTDM_IRQTYPE_EDGE,
+	                 IRQ_TYPE_EDGE_FALLING,
 	                 THIS_MODULE->name, NULL)) != 0) {
 
         //printk("irq request , IRQTYPE_EDGE\n");
@@ -72,6 +79,7 @@ static int __init exemple_init (void)
 		gpio_free(GPIO_IN);
 		return err;
 	}
+    printk("globalCounter %d\n",globalCounter);
     printk("done \n");
 
 	return 0; 
@@ -81,6 +89,7 @@ static int __init exemple_init (void)
 
 static void __exit exemple_exit (void)
 {
+    printk("globalCounter %d\n",globalCounter);
 	rtdm_irq_free(& irq_rtdm);
 	gpio_free(GPIO_OUT);
 	gpio_free(GPIO_IN);
