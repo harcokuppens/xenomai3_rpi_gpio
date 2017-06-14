3bf1b958cc2fac4a29f0eb0b0c85f6a44b32c1c1
Harco Kuppens <h.kuppens@gmail.com>
2017-06-14 12:01:05 +0000

    improve examples code mainly improved debouncingw

--- examples/xenomai3/userspace_programs/isr/isr.c
+++ examples/xenomai3/userspace_programs/isr/isr.c
@@ -30,69 +30,15 @@ RT_TASK isr_task;
  
 #define VALUE_MAX 33
 static	int fd_in=0;
-static	int fd_out_write=0;
-static	int fd_out_read=0;
 
 
-static int GPIOWriteInit(int pin, int value) {
- 
-	int fd=0;
-	char path[VALUE_MAX];
-    snprintf(path, VALUE_MAX, "/dev/rtdm/pinctrl-bcm2835/gpio%d", pin);
-	rt_printf("open device wronly: %s   ---\n",path );
-	fd = open(path, O_WRONLY);
-	//fd = open(path, O_RDWR|O_NONBLOCK); // cannot read from filehandle even with O_RDWR
-	//fd = open(path, O_RDWR); // cannot read from filehandle even with O_RDWR
-	if (-1 == fd) {
-        int errsv = errno;
-        rt_printf("Failed to failed to gpio pin %d , return %d errno %d\n", pin,fd,errsv);
-		return(-1);
-	}
-    rt_printf("Succes to open pin %d\n", pin);
 
-	rt_printf("set device output mode: %s   ---\n",path );
-    int ret =ioctl(fd, GPIO_RTIOC_DIR_OUT, &value);
-    int errsv = errno;
-    if (ret) {
-        rt_printf("Failed to set output mode to pin %d  ret %d errno %d\n", pin,ret,errsv);
-        return ret;
-    }
-    rt_printf("Succes to set output mode to pin %d\n",pin);
-    
-    return fd;
-}
-
-static int GPIOReadInit(int pin) {
-	int fd=0;
-     
-	char path[VALUE_MAX];
-    snprintf(path, VALUE_MAX, "/dev/rtdm/pinctrl-bcm2835/gpio%d", pin);
-	rt_printf("open device rdonly: %s   ---\n",path );
-	fd = open(path, O_RDONLY|O_NONBLOCK);
-	if (-1 == fd) {
-        int errsv = errno;
-        rt_printf("Failed to failed to gpio pin %d , return %d errno %d\n", pin,fd,errsv);
-		return(-1);
-	}
-    rt_printf("Succes to open pin %d \n", pin);
-
-	rt_printf("set device input mode: %s   ---\n",path );
-    int ret =ioctl(fd, GPIO_RTIOC_DIR_IN);
-    int errsv = errno;
-    if (ret) {
-        rt_printf("Failed to set input mode to pin %d  ret %d errno %d\n", pin,ret,errsv);
-        return ret;
-    }
-    rt_printf("Succes to set input mode to pin %d\n",pin);
-    
-    return fd;
-}
 
 static int GPIOInterruptInit(int pin) {
 	int fd=0;
 	int do_select = 0;
-    int trigger = GPIO_TRIGGER_LEVEL_LOW;
-	//int	trigger = GPIO_TRIGGER_EDGE_FALLING;
+    //int trigger = GPIO_TRIGGER_LEVEL_LOW;
+	int	trigger = GPIO_TRIGGER_EDGE_FALLING;
 /*
 	} trigger_types[] = {
 		{ .name = "edge", .flag = GPIO_TRIGGER_EDGE_RISING },
@@ -125,151 +71,60 @@ static int GPIOInterruptInit(int pin) {
 		return ret;
 	}
 
-    int  value;	
-	fd_set set;
-
-	FD_ZERO(&set);
-	FD_SET(fd, &set);
-
-    do_select=0;
-	for (;;) {
-		if (do_select) {
-			ret = select(fd + 1, &set, NULL, NULL, NULL);
-			if (ret < 0) {
-				ret = -errno;
-				rt_printf("failed listening to gpio pin %d [%s]\n", pin, symerror(ret));
-				return ret;
-			}
-		}
-		ret = read(fd, &value, sizeof(value));
-		if (ret < 0) {
-			ret = -errno;
-			rt_printf("failed reading from gpio pin %d [%s]\n", pin, symerror(ret));
-			return ret;
-		}
-		rt_printf("received irq, GPIO state=%d\n", value);
-        //rt_timer_spin(500000000);
-        rt_timer_spin(2000000000);
-	}
-
-	//close(fd);
-
-
-
-
-
     return fd;
 }
 
-static int GPIOWrite(int fd, int pin, int value) {
 
-    rt_printf("write  value %d to pin %d \n", value, pin);
-    int ret = write(fd, &value, sizeof(value));
-    int errsv = errno;
-    if (sizeof(value) != ret ) {
-        rt_printf("Failed to write  value %d to pin %d  ret %d errno %d\n", value, pin,ret,errsv);
-		return(-1);
-	}
-	return(0);
-}
-
-static int GPIORead(int fd, int pin) {
-    int value;
-    //rt_printf("Try to read input pin %d \n",pin);
-    int ret = read(fd, &value, 4);
-    int errsv = errno;
-	if (-1 == ret ) {
-        rt_printf("Failed to read  value %d to pin %d  ret %d errno %d\n", value, pin,ret,errsv);
-		return(-1);
-	}
-    //rt_printf("Succes to read input pin %d with value %d\n",pin,value);
-    rt_printf("read input pin %d with value %d\n",pin,value);
- 
- 
-	return(0);
-}
-
-void blink( int repeat, int timeout) {
-	rt_printf("blink(%d, %d)\n", repeat, timeout);
-	do {
-		rt_printf("Blink now\n");
-	    GPIORead(fd_in,PIN);
-		GPIOWrite(fd_out_write,POUT, repeat % 2);
-	    //GPIORead(fd_out_write,POUT); -> gives error, instead make also read handle for same pin
-	    GPIORead(fd_out_read,POUT);
-        //rt_task_sleep(1000000000);
-		//rt_timer_spin(1000000000);
-        rt_timer_spin(500000000);
-	} while (repeat--);
-	rt_printf("Done blinking!\n");
-}
-
-
-
-void runBlink(void *arg) {
-	rt_printf("Starting blink...\n");
-    // loop 10 times to toggle between 0 and 1 value  => 5 blinks
-    // period:  1 second   => duration light on or off
-	blink( 10, 1);
-}
-
-/* dummy task with higher prio */
-void runHello(void *args) {
-	int count = 5;
-	int waitTime = *(int*)args;
-	rt_printf("Starting hello world task... should be printed after blinking done!!\n");
-	do {
-		rt_printf("Hello world!\n");
-        rt_task_sleep(500000000);
-	} while (--count);
-	rt_printf("Done running!\n");
-}
-
-// task to start tasks
-void runStartTasks(void *args) {
-	int period = 100;
-	rt_task_start(&blink_task, &runBlink, NULL);
-	rt_task_start(&hello_task, &runHello, &period);
-}
-
-int run() {
-    // NOTE: initialization code is within linux : none-realtime  => should be done in advance
-	fd_in=GPIOReadInit(PIN);
-    // configure for single pin both an input as output file handle
-    // to be able to both write and read it !! (instead of O_RDWR which doesn't work for reading)  
-	fd_out_read=GPIOReadInit(POUT);
-	fd_out_write=GPIOWriteInit(POUT, 1);
-
-	rt_task_create(&blink_task, "BlinkTask", 0, 50, 0);
-	rt_task_create(&hello_task, "Task", 0, 40, 0);
-        
-        // task to start tasks
-	rt_task_create(&startTasks, "StartTask", 0, 99, 0);
-	rt_task_start(&startTasks, &runStartTasks, NULL);
-}
 
 
 void runIsr(void *args) {
     // NOTE: initialization code is within linux : none-realtime  => should be done in advance
-	fd_in=GPIOInterruptInit(PIN); 
+    int value;	
+    int ret;
+    int fd=fd_in;
+    int pin=PIN;
+    int switch_count=0;
+    int count=0;
+    RTIME lasttime,now,lastbounce;
+    lasttime =rt_timer_read();
+    lastbounce=lasttime;
+	for (;;) {
+     
+        // wait for interrupt 
+       	ret = read(fd, &value, sizeof(value));
+        count++;
+		if (ret < 0) {
+			ret = -errno;
+			rt_printf("failed reading from gpio pin %d [%s]\n", pin, symerror(ret));
+			break;
+		}
+     
+        // handle interrupt
+        now =rt_timer_read();
+        if ( now > lasttime + 300000000) {
+            // new interrupt (no bounce)  
+    		rt_printf("bounce time=%llu\n",lastbounce-lasttime);
+            lasttime =now;
+         
+            // on new interrupt we assume switch is triggered and we toggle output 
+    		rt_printf("\nINTERRUPT: received irq, GPIO state=%d count=%d\n", value,count);
+            switch_count++;
+        } else {
+            // bounce: ignore  bounce interrupts
+            lastbounce=now;
+   	        printf("BOUNCE: count=%d, time=%llu\n", count, now);
+        } 
+	}
 }
 
 int main(int argc, char *argv[]) {
 	 	
+	fd_in=GPIOInterruptInit(PIN); 
     rt_task_create(&isr_task, "IsrTask", 0, 40, 0);
 	rt_task_start(&isr_task, &runIsr, NULL);
  	getchar();
  	
+    close(fd_in);
 	return(0);
 }
 
-int main1(int argc, char *argv[]) {
-	 	
- 	run();
-    close(fd_in);
-    close(fd_out_write);
-    close(fd_out_read);
- 	getchar();
- 	
-	return(0);
-}
